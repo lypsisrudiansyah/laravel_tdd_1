d51f7a0c08ec0559c91ff45c48e9bc907e148bf0
lypsisrudiansyah <rudiansyahether@gmail.com>
2023-06-02 20:02:03 +0700

    🔥 chore(.gitignore): add .env.testing to gitignore 🔨 refactor(api.php): import TodoListController and remove unused code 🚨 test(TodoListTest.php): add RefreshDatabase trait and create TodoList in test 🚨 test(TodoListTest.php): remove commented code and add dd() for debugging 🚨 test(TestCase.php): add setUp() method to disable exception handling during tests

--- tests/Feature/TodoListTest.php
+++ tests/Feature/TodoListTest.php
@@ -2,12 +2,15 @@
 
 namespace Tests\Feature;
 
+use App\Models\TodoList;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class TodoListTest extends TestCase
 {
+    use RefreshDatabase ;
+    
     /**
      * A basic feature test example.
      *
@@ -18,11 +21,11 @@ class TodoListTest extends TestCase
         /* $response = $this->get('/');
 
         $response->assertStatus(200); */
+        TodoList::create(['name' => 'My Todo List', 'user_id' => 1]);
         
-        $this->withoutExceptionHandling();
-
         $response = $this->getJson('api/todo-list');
-        // dd($response->json());
+
+        dd($response->getContent());
         $this->assertEquals(1, count($response->json()));
     }
 }
