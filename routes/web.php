87b13ce93728e912146afec49ee2d297a23b87a5
lypsisrudiansyah <rudiansyahether@gmail.com>
2023-07-05 22:15:37 +0700

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Google\Client;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/drive', function () {
+    $client = new Client();
+    $client->setAuthConfig(storage_path('app/credentials.json'));
 });
